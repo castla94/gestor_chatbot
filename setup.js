12ca42e963b9de36c0be9140cdb2ec7fe2276748
castla94 <castla1994@gmail.com>
2025-05-20 17:31:39 -0400

    fix johana

--- setup.js
+++ setup.js
@@ -344,7 +344,7 @@ app.post('/clientes/reset-johana', async (req, res) => {
     try {
         const clientPath = path.join(clientsBasePath, `cliente_johannarubiocoppola`);
         process.chdir(clientPath);
-        execSync('pm2 delete bot-johannarubiocoppola', { stdio: 'inherit' });
+        execSync('pm2 delete bot-johannarubiocoppola || true', { stdio: 'inherit' });
         execSync('pm2 start app.js --name="bot-johannarubiocoppola" --max-memory-restart=3G', { stdio: 'inherit' });
         logger.info('Reboot bot-johannarubiocoppola exitoso');
         res.status(200).json({ message: `Reboot exitoso` });
