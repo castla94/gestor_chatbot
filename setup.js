cb9f31a1d2d766fa1d7e5818ad95ff6104242f3e
castla94 <castla1994@gmail.com>
2025-06-09 23:34:18 -0400

    update

--- setup.js
+++ setup.js
@@ -348,8 +348,8 @@ app.post('/clientes/reset-johana', async (req, res) => {
 /*
         const clientPath2 = path.join(clientsBasePath, `cliente_contacto`);
         process.chdir(clientPath2);
-        execSync('bash ./start-pm2.sh bot-contacto', { stdio: 'inherit' });*/
-/*
+        execSync('bash ./start-pm2.sh bot-contacto', { stdio: 'inherit' });
+
         execSync('pm2 delete bot-johannarubiocoppola || true', { stdio: 'inherit' });
         execSync('pm2 start app.js --name="bot-johannarubiocoppola" --max-memory-restart=3G', { stdio: 'inherit' });
         */
